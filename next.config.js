5f5b475b317e95188e35f9d665cfb76c1d83c563
Krustycoin0 <133280489+Krustycoin0@users.noreply.github.com>
2023-06-20 20:11:26 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,17 +1,10 @@
 /** @type {import('next').NextConfig} */
-
 const nextConfig = {
-    reactStrictMode: true,
-    webpackDevMiddleware: (config) => {
-        config.watchOptions = {
-            poll: 1000,
-            aggregateTimeout: 300,
-        };
-        return config;
-    },
-    images: {
-        domains: ['polyplace.infura-ipfs.io'],
-    },
+	reactStrictMode: true,
+	swcMinify: true,
+	images: {
+		domains: ['infura-ipfs.io', 'https://app.infura.io/dashboard/ethereum/d57f136bd5024f4889c74a1d4f7ce760/settings/endpoints'],
+	},
 };
 
 module.exports = nextConfig;
