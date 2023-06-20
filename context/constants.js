175388bded91b540e2a9328c27ef062dce9406c4
Krustycoin0 <133280489+Krustycoin0@users.noreply.github.com>
2023-06-20 19:21:07 +0200

    Update and rename constants.js to constants.ts

--- context/constants.js
+++
@@ -1,4 +0,0 @@
-import marketAbi from './NFTMarketplace.json';
-
-export const MarketAddress = '0x848c326254fC9b54dfC9e21c97a40a31370E9ce0';
-export const MarketAddressABI = marketAbi.abi;
---
+++ context/constants.ts
@@ -0,0 +1,5 @@
+import market from './NFTMarketplaceTest3.json';
+
+export const MarketAddress = '0x848c326254fC9b54dfC9e21c97a40a31370E9ce0';
+export const MarketAddressAbi = market.abi;
+
