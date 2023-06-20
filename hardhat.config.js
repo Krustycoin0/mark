4efeb99df457447890a1550ac2ae9d6e15060a59
Krustycoin0 <133280489+Krustycoin0@users.noreply.github.com>
2023-06-20 20:08:27 +0200

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,18 +1,49 @@
-const fs = require('fs');
+// const fs = require('fs');
+// require('@nomicfoundation/hardhat-toolbox');
 
-require('@nomiclabs/hardhat-waffle');
+// const privateKey = fs.readFileSync('.secret').toString().trim();
 
-const privateKey = fs.readFileSync('.secret').toString().trim();
+// /** @type import('hardhat/config').HardhatUserConfig */
+// module.exports = {
+//   defaultNetwork: 'polygon_mumbai',
+//   networks: {
+//     hardhat: {},
+//     polygon_mainnet: {
+//       url: 'https://polygon-mainnet.g.alchemy.com/v2/S-7W-EuKWAo_ksZu7lx0Rn0ySh0pM3kC',
+//       accounts: [`0x${privateKey}`],
+//     },
+//   },
+//   solidity: '0.8.9',
+// };
+
+// Production Configuration
+require('@nomiclabs/hardhat-ethers');
 
 module.exports = {
-  networks: {
-    hardhat: {
-      chainId: 137,
-    },
-    matic: {
-      url: 'https://polygon-rpc.com',
-      accounts: [privateKey],
-    },
-  },
-  solidity: '0.8.4',
+	defaultNetwork: 'matic',
+	networks: {
+		hardhat: {},
+		matic: {
+			url: process.env.NEXT_PUBLIC_ALCHEMY_API_URL,
+			accounts: [process.env.NEXT_PUBLIC_WALLET_PRIVATE_KEY],
+		},
+	},
+	solidity: {
+		version: '0.8.9',
+		settings: {
+			optimizer: {
+				enabled: true,
+				runs: 200,
+			},
+		},
+	},
+	paths: {
+		sources: './contracts',
+		tests: './test',
+		cache: './cache',
+		artifacts: './artifacts',
+	},
+	mocha: {
+		timeout: 20000,
+	},
 };
